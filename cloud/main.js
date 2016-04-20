81d6e9eaa3941f65999a825e0b4edf20e886220e
drchris1502 <drchris@sent.com>
2016-04-19 21:53:01 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -222,7 +222,7 @@ Parse.Cloud.beforeSave("Pairing", function(request,response) {
     userQuery.find({
       success: function(invitedUser) {
       	console.log("Before Send.");
-      	console.log(invitedUser);
+      	console.log(invitedUser.id);
       	send(invitedUser.id, "", "You have a pairing invitation!", false);
       	console.log("Sent pariring push.");
       }	
