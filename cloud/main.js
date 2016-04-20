c69f9b77c9e1233b7b4bf5af810124938a16eddf
drchris1502 <drchris@sent.com>
2016-04-19 21:28:56 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -214,6 +214,7 @@ Parse.Cloud.beforeSave("Pairing", function(request,response) {
     });
   }).then(function() {
     // if the invited user has an account already, send a push
+    console.log("start");
     var userQuery = new Parse.Query(request.user);
     userQuery.equalTo("email", toUserEmail);
     console.log("Before Query.");
