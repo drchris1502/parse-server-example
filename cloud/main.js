c5c0284eabd48def566d0fd8b0a26c4164da8ca1
drchris1502 <drchris@sent.com>
2017-04-10 22:10:49 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -94,13 +94,10 @@ if (request.object.get('pushed') === false) {
     var query = new Parse.Query(Parse.Installation);
     query.equalTo('user', request.object.get('toId'));  // push to the right user!!!
     var partner = request.object.get('toId');
-    var userQuery = new Parse.Query(request.user);
+    var userQuery = new Parse.Query(Parse.User);
     var soundFile = 'default'
-    userQuery.get(partner).then (
-    function(partnerUser) {
+    userQuery.get(partner).then (function(partnerUser) {
        soundFile = partnerUser.get("soundFile") + '.caf';
-    }, function (error) {
-       console.log(error);
     }).then (function() {
        send(request.object.get('toId'), soundFile, "You have a new message!", false).then(
        function(object) {
@@ -129,6 +126,7 @@ if (request.object.get('pushed') === false) {
     response.success();  // save the message as normal.
 }
 });
+
 
 
 
